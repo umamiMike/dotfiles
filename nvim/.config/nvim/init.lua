bee613d84c95eab8f8bb9d41b1bf6672bb0355cc
Mike Wilding <heymichaelwilding@gmail.com>
2026-07-09 09:12:00 -0700

    further updates wsl

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -598,45 +598,8 @@ require('lazy').setup({
       vim.cmd.colorscheme 'onedark'
     end,
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-    opts = {
-      ensure_installed = {
-        'python',
-        'typescript',
-        'javascript',
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-  },
-
   require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
