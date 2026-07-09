3ca155a874091050d5abf7ab016fce1759874549
Mike Wilding <heymichaelwilding@gmail.com>
2026-07-09 14:29:54 -0700

    updates wsl config

--- home-manager/.config/home-manager/wsl.nix
+++ home-manager/.config/home-manager/wsl.nix
@@ -3,16 +3,5 @@
   home.homeDirectory = "/home/mikew";
   home.stateVersion = "24.11";
 
-  home.packages = with pkgs; [
-    # colima
-    # devenv
-    # gcc-unwrapped
-    # git-lfs
-    # google-cloud-sdk
-    # nil
-    # syncthing
-    # tealdeer
-    # tmux
-   # tmuxinator
-  ];
+  home.packages = with pkgs; [ ];
 }
