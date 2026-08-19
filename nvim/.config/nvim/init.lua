0691b8fd3c5355c2cc03a08f7c63ec65b457a3b1
Mike Wilding <heymichaelwilding@gmail.com>
2026-08-19 12:16:53 -0700

    Split diagnostics/document-symbols keymap, apply ivy theme to search pickers
    <leader>sD now runs workspace diagnostics; <leader>sd is new and runs LSP
    document symbols. Every <leader>s* telescope picker plus <leader><leader>
    (buffers) now opens with the ivy theme via a shared get_ivy alias.
    keymaps.md updated to match.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -206,28 +206,52 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      local get_ivy = require('telescope.themes').get_ivy
+      vim.keymap.set('n', '<leader>sh', function()
+        builtin.help_tags(get_ivy())
+      end, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', function()
+        builtin.keymaps(get_ivy())
+      end, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files(get_ivy())
+      end, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', function()
+        builtin.builtin(get_ivy())
+      end, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', function()
+        builtin.grep_string(get_ivy())
+      end, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep(get_ivy())
+      end, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sD', function()
+        builtin.diagnostics(get_ivy())
+      end, { desc = '[S]earch [D]iagnostics (workspace)' })
+      vim.keymap.set('n', '<leader>sd', function()
+        builtin.lsp_document_symbols(get_ivy())
+      end, { desc = '[S]earch [D]ocument Symbols' })
+      vim.keymap.set('n', '<leader>sr', function()
+        builtin.resume(get_ivy())
+      end, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', function()
+        builtin.oldfiles(get_ivy())
+      end, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        builtin.buffers(get_ivy())
+      end, { desc = '[ ] Find existing buffers' })
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
       vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
+        builtin.live_grep(get_ivy {
           grep_open_files = true,
           prompt_title = 'Live Grep in Open Files',
-        }
+        })
       end, { desc = '[S]earch [/] in Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+        builtin.find_files(get_ivy { cwd = vim.fn.stdpath 'config' })
       end, { desc = '[S]earch [N]eovim files' })
 
       -- My Custom ones moved over
@@ -413,6 +437,7 @@ require('lazy').setup({
                 useLibraryCodeForTypes = true,
               },
               venvPath = '.',
+              pythonPath = vim.fn.has 'wsl' == 1 and '/mnt/c/Users/mikew/AppData/Local/refuge/pipeline/bin/python/python.exe' or nil,
             },
           },
         },
@@ -454,7 +479,7 @@ require('lazy').setup({
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
-    cmd = { 'ConformInfo', 'Format' },
+    cmd = { 'ConformInfo' },
     keys = {
       {
         '<leader>f',
@@ -500,7 +525,7 @@ require('lazy').setup({
         -- Conform can also run multiple formatters sequentially
         python = { 'ruff_fix', 'ruff_format' },
         cpp = { 'clang-format' },
-        --
+        markdown = { 'prettierd', 'prettier', stop_after_first = true },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
@@ -596,7 +621,7 @@ require('lazy').setup({
   { import = 'custom.plugins' },
 }, {
   git = {
-    cmd = vim.fn.getenv('WSL_DISTRO_NAME') ~= vim.NIL and '/usr/bin/git' or 'git',
+    cmd = vim.fn.getenv 'WSL_DISTRO_NAME' ~= vim.NIL and '/usr/bin/git' or 'git',
   },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -683,4 +708,3 @@ vim.keymap.set('v', '<leader>p', insert_print_from_visual, { silent = true, desc
 if vim.fn.has 'wsl' == 1 then
   vim.g.python3_host_prog = '/mnt/c/Users/mikew/AppData/Local/refuge/pipeline/bin/python/python.exe'
 end
-
